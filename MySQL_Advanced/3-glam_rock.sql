c7f847d68874017d945d0e8db413b2f6248984e3
AslanBayona <aslan.hernandez@anahuac.mx>
2026-02-13 09:21:32 -0600

    Update 3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,4 @@
 SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+ORDER BY lifespan DESC;
